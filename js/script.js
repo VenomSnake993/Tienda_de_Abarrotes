b7405c4078e3b7ffa69a7d50b70e334b074cf109
VenomSnake993 <203107113@cuautitlan.tecnm.mx>
2026-02-14 14:28:24 -0600

    Se añadio el carrusel ininito mejorado.

--- js/script.js
+++ js/script.js
@@ -13,29 +13,4 @@ function ColorBarra() {
     })
 }
 
-function CarruselPromociones() {
-    const contenedorPromociones = document.querySelector(".promociones");
-    const promociones = contenedorPromociones.children;
-    let direccion = 1;
-
-    for (let index = 0; index < promociones.length; index++) {
-        promociones[index].addEventListener("mouseover", () => { direccion = 0; });
-        promociones[index].addEventListener("mouseout", () => { direccion = 1});
-    }
-    
-    setInterval(() => {
-        let scrollActual = contenedorPromociones.scrollLeft;
-        const scrollMax = contenedorPromociones.scrollWidth - contenedorPromociones.clientWidth;
-        // console.log(scrollActual, scrollMax)
-        if (scrollActual >= scrollMax) {
-            direccion = -1;
-        } else if (scrollActual <= 0) {
-            direccion = 1;
-        }
-        contenedorPromociones.scrollBy(direccion, 0);
-
-    }, 16);
-}
-
 ColorBarra();
-CarruselPromociones();
