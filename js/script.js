c476c683ed44bf5c01079fcdabb731540721698c
VenomSnake993 <203107113@cuautitlan.tecnm.mx>
2026-02-18 12:33:43 -0600

    Se añádio funcionalidad al formulario de contacto.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// import emailjs from "/@emailjs/browser";
+
 function ColorBarra() {
     window.addEventListener("scroll", () => {
         const barraNavegacion = document.querySelector(".barra-navegacion");
@@ -10,7 +12,38 @@ function ColorBarra() {
         } else if (!posicionScroll) {
             barraNavegacion.classList.remove("activar-color-barra-navegacion");
         }
-    })
+    });
 }
 
+function EnviarCorreo() {
+    const formulario = document.querySelector(".form-contacto");
+    formulario.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const datosFormulario = Object.fromEntries(new FormData(formulario));
+        const nombrePersona = datosFormulario.nombreCompleto;
+        const correo = datosFormulario.correo;
+        const asunto = datosFormulario.asunto;
+        const mensaje = datosFormulario.mensaje;
+
+        const parametrosCorreo = {
+            name: nombrePersona,
+            email: correo,
+            title: asunto,
+            message: mensaje
+        };
+
+        emailjs.init({ publicKey: "q3nXe68EI_Jlh8yMg" });
+        emailjs.send("gmail_service_abarrotes", "template_lpj89n3", parametrosCorreo)
+            .then((response) => {
+                console.log("SUCCESS!", response.status, response.text);
+                formulario.reset();
+                alert("Correo enviado correctamente, te tendremos en cuenta, GRACIAS. 💖");
+            },
+                (error) => {
+                    console.log("FAILED...", error);
+                    alert("Error al enviar el correo, por favor intenta nuevamente. 😔");
+                });
+    });
+}
+EnviarCorreo();
 ColorBarra();
